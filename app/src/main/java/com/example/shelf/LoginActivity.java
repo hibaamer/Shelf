d593ab973fb7d4c51f5e8ae0586a7a761b672043
ftn999 <62928470+ftn999@users.noreply.github.com>
2020-04-02 17:15:17 +0300

    Merge branch 'master' of C:\Users\HP\AndroidStudioProjects\Shelf with conflicts.

---
+++ app/src/main/java/com/example/shelf/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.shelf;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState); // load the layout
+       setContentView(R.layout.activity_login);
+
+    }
+
+
+}
