e23312bb81e662dd76556f99e5609c8d1c8ad49a
hibaamer <bebalolo11@gmail.com>
2020-04-15 08:43:15 +0300

    ..

--- app/src/main/java/com/example/shelf/search.java
+++ app/src/main/java/com/example/shelf/search.java
@@ -3,14 +3,12 @@ package com.example.shelf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class search extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_ctivity);
+        setContentView(R.layout.activity_search);
     }
 }
