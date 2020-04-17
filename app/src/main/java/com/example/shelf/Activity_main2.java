e3a45d7f8eea0dd0aaa5c0dfbb2424419a505879
hibaamer <bebalolo11@gmail.com>
2020-04-17 07:37:41 +0300

    search

--- app/src/main/java/com/example/shelf/Activity_main2.java
+++ app/src/main/java/com/example/shelf/Activity_main2.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class search extends AppCompatActivity {
+public class Activity_main2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_main2);
     }
 }
